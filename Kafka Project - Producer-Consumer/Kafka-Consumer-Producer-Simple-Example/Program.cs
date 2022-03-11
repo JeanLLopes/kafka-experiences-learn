b407d066c46afe98bd0b2b911257a3857582add0
Jean de Lima Lopes <jean.llopes@outlook.com>
2022-03-11 09:56:48 +0000

    add lib confluent kafka

--- Kafka Project - Producer-Consumer/Kafka-Consumer-Producer-Simple-Example/Program.cs
+++ Kafka Project - Producer-Consumer/Kafka-Consumer-Producer-Simple-Example/Program.cs
@@ -1,12 +1,34 @@
-﻿using System;
+﻿using Confluent.Kafka;
 
 namespace MyApp // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Starting the application...");
+
+            var config = new ProducerConfig 
+            { 
+                BootstrapServers = "rocket-01.srvs.cloudkafka.com:9094,rocket-02.srvs.cloudkafka.com:9094,rocket-03.srvs.cloudkafka.com:9094",
+                SecurityProtocol = SecurityProtocol.SaslSsl,
+                SaslMechanism = SaslMechanism.Plain,                
+                SaslUsername = "32e9tpoh",
+                SaslPassword = ""
+            };
+
+            using (var p = new ProducerBuilder<Null, string>(config).Build())
+            {
+                try
+                {
+                    var dr = await p.ProduceAsync("32e9tpoh-default", new Message<Null, string> { Value="test" });
+                    Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}'");
+                }
+                catch (ProduceException<Null, string> e)
+                {
+                    Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+                }
+            }
         }
     }
 }
